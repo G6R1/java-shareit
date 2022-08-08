ac575c45e30268c62898649670f581558f233b16
GRN <Gerian109@yandex.ru>
2022-08-08 17:46:46 +0300

    save 08 08

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -4,21 +4,33 @@ import lombok.*;
 import ru.practicum.shareit.requests.model.ItemRequest;
 import ru.practicum.shareit.user.model.User;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Data
+@Entity
+@Table(name = "items")
 public class Item {
+    @Id
+    @Column(name = "item_id")
     private Long id;
     @NotNull
     @NotBlank
+    @Column(name = "name")
     private String name;
     @NotNull
     @NotBlank
+    @Column(name = "description")
     private String description;
     @NotNull
+    @Column(name = "available")
     private Boolean available; //статус о том, доступна или нет вещь для аренды
     private User owner;
     //если вещь была создана по запросу другого пользователя,
--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -2,21 +2,32 @@ package ru.practicum.shareit.user.model;
 
 import lombok.*;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Data
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @Column(name = "user_id")
     private Long id;
     @NotNull
     @NotBlank
+    @Column(name = "name")
     private String name;
     @Email
     @NotNull
     @NotBlank
+    @Column(name = "email")
     private String email; //учтите, что два пользователя не могут иметь одинаковый адрес электронной почты
 }
